2b94b1e58c8c763fe859bd9794088e6b4371cf06
Kamil Pyszniak <x.pyszniak@gmail.com>
2024-11-06 20:22:59 +0100

    CreateEvent comp

--- src/Components/CreateEvent.jsx
+++ src/Components/CreateEvent.jsx
@@ -1,11 +1,19 @@
 import './create-event.css'
 
-
-
 const CreateEvent = () => {
-  return (
-    <div>CreateEvent</div>
-  )
+	return (
+		<div className='ce-container'>
+			<h1>Create a new event!</h1>
+			<hr />
+			<label htmlFor='hours'>Time</label>
+			<input type='number' name='hours' min={0} max={24} />
+			<label htmlFor='title'>Title</label>
+			<input type='text' name='title' />
+			<label htmlFor='description'>Description</label>
+			<textarea name='description'></textarea>
+			<button>Create Event</button>
+		</div>
+	)
 }
 
 export default CreateEvent
--- src/Components/SelectedDay.jsx
+++ src/Components/SelectedDay.jsx
@@ -1,4 +1,5 @@
-import './selected-day.css'
+import { useState } from 'react'
+import CreateEvent from './CreateEvent.jsx'
 
 const months = [
 	'January',
@@ -15,10 +16,11 @@ const months = [
 	'December',
 ]
 
+import './selected-day.css'
 const SelectedDay = ({ selectedDay }) => {
+	const [showPopup, setShowPopup] = useState(false)
 	const arrDate = selectedDay.split('.') // [0] - day, [1] - month, [2] - year
 
-	
 	return (
 		<>
 			<div className='sd-container'>
@@ -28,61 +30,71 @@ const SelectedDay = ({ selectedDay }) => {
 						<h2>{months[arrDate[1]]}</h2>
 						<h2>{arrDate[2]}</h2>
 					</div>
-					<button className='btn-create-event'>Create Event</button>
+					<button
+						className='btn-create-event'
+						onClick={() => {
+							setShowPopup(true)
+						}}>
+						Create Event
+					</button>
 				</div>
-				<div className='sd-events'>
-					<div className='sd-events-inner'>
-						<div className='sd-event'>
-							<h2>12:00</h2>
-							<div className='sd-event-content'>
-								<h3>Title</h3>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel tempora voluptatem nulla in
-									iste porro nihil vitae. At, vero! Ipsam, tenetur corrupti harum exercitationem consectetur modi beatae
-									eligendi iure provident non nesciunt at tempora, laudantium dolorem accusantium fuga ex.
-								</p>
+				{showPopup ? (
+					<CreateEvent />
+				) : (
+					<div className='sd-events'>
+						<div className='sd-events-inner'>
+							<div className='sd-event'>
+								<h2>12:00</h2>
+								<div className='sd-event-content'>
+									<h3>Title</h3>
+									<p>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel tempora voluptatem nulla in
+										iste porro nihil vitae. At, vero! Ipsam, tenetur corrupti harum exercitationem consectetur modi
+										beatae eligendi iure provident non nesciunt at tempora, laudantium dolorem accusantium fuga ex.
+									</p>
+								</div>
+								<div className='sd-buttons'>
+									<button>
+										<i className='bx bx-edit-alt'></i>
+									</button>
+									<button>
+										<i className='bx bx-x'></i>
+									</button>
+								</div>
 							</div>
-							<div className='sd-buttons'>
-								<button>
-									<i className='bx bx-edit-alt'></i>
-								</button>
-								<button>
-									<i className='bx bx-x'></i>
-								</button>
+							<div className='sd-event'>
+								<h2>12:00</h2>
+								<div className='sd-event-content'>
+									<h3>Title</h3>
+									<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, totam!</p>
+								</div>
+								<div className='sd-buttons'>
+									<button>
+										<i className='bx bx-edit-alt'></i>
+									</button>
+									<button>
+										<i className='bx bx-x'></i>
+									</button>
+								</div>
 							</div>
-						</div>
-						<div className='sd-event'>
-							<h2>12:00</h2>
-							<div className='sd-event-content'>
-								<h3>Title</h3>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, totam!</p>
-							</div>
-							<div className='sd-buttons'>
-								<button>
-									<i className='bx bx-edit-alt'></i>
-								</button>
-								<button>
-									<i className='bx bx-x'></i>
-								</button>
-							</div>
-						</div>
-						<div className='sd-event'>
-							<h2>12:00</h2>
-							<div className='sd-event-content'>
-								<h3></h3>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, dolorem!</p>
-							</div>
-							<div className='sd-buttons'>
-								<button>
-									<i className='bx bx-edit-alt'></i>
-								</button>
-								<button>
-									<i className='bx bx-x'></i>
-								</button>
+							<div className='sd-event'>
+								<h2>12:00</h2>
+								<div className='sd-event-content'>
+									<h3></h3>
+									<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, dolorem!</p>
+								</div>
+								<div className='sd-buttons'>
+									<button>
+										<i className='bx bx-edit-alt'></i>
+									</button>
+									<button>
+										<i className='bx bx-x'></i>
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</>
 	)
