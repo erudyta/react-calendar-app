8880eaee7618470efd71e4e1110a6d48a14a3129
Kamil Pyszniak <x.pyszniak@gmail.com>
2024-11-12 20:35:55 +0100

    added RWD

---
+++ README.md
@@ -0,0 +1,36 @@
+# Calendar app.
+The calendar application allows you to save scheduled activities for a specific day. When you click on a selected day in the calendar, a list of events falling on that day will be displayed. If a given day has no events created there is an option to create an event by clicking the appropriate button. The created event can be deleted and its content can be freely modified.
+
+## Built with
+Project is created with:
+* HTML
+* CSS
+* JavaScript
+* React
+
+## Setup
+##### Prerequisites:
+You are required to have the following things installed on your desktop:
+* <a href="https://nodejs.org/en/download/package-manager" target="_blank">Node.js </a>
+* <a href="https://git-scm.com/downloads" target="_blank">Git </a>
+
+##### Run project:
+Once you have these prerequisites installed, you can follow these steps to download a React project from Github and run it on your computer:
+1. Open your terminal and clone repository.
+```
+$ git clone {the url to the GitHub repo}
+```
+2. Navigate to the project directory:
+```
+cd <repo-name>
+```
+3. Install dependencies.
+```
+npm install
+```
+4. Start the development server.
+```
+npm start
+```
+
+
--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -39,36 +39,37 @@ const Calendar = ({
 	return (
 		<div className='calendar-container'>
 			<h1>Calendar</h1>
-			<div className='calendar-month-year'>
-				<h2>{months[currMonth]}, </h2>
-				<h2> {currYear}</h2>
-				<div>
-					<button onClick={onMinusMonth}>
-						<i className='bx bx-left-arrow-alt'></i>
-					</button>
-					<button onClick={onPlusMonth}>
-						<i className='bx bx-right-arrow-alt'></i>
-					</button>
+			<div>
+				<div className='calendar-month-year'>
+					<div>
+						<h2>{months[currMonth]}, </h2>
+						<h2> {currYear}</h2>
+					</div>
+					<div>
+						<button onClick={onMinusMonth}>
+							<i className='bx bx-left-arrow-alt'></i>
+						</button>
+						<button onClick={onPlusMonth}>
+							<i className='bx bx-right-arrow-alt'></i>
+						</button>
+					</div>
 				</div>
-			</div>
-			<div className='calendar-daysofweek'>
-				{days.map(day => (
-					<span key={day}>{day}</span>
-				))}
-			</div>
-			<div className='calendar-daysofmonth'>
-				{arrBlankPlacesInCalendar.map((blank, index) => (
-					<button key={index} className='calendar-daysofmonth-hidden'></button>
-				))}
-				{arrNumberOfDaysInMonth.map((day, dayNumber) => {
-					const dateToCompare = `${dayNumber + 1}.${currMonth}.${currYear}`
-					console.log(dateToCompare)
-					const listOfEventsInDay = events.filter(event => event.day === dateToCompare)
-					console.log(listOfEventsInDay)
+				<div className='calendar-daysofweek'>
+					{days.map(day => (
+						<span key={day}>{day}</span>
+					))}
+				</div>
+				<div className='calendar-daysofmonth'>
+					{arrBlankPlacesInCalendar.map((blank, index) => (
+						<button key={index} className='calendar-daysofmonth-hidden'></button>
+					))}
+					{arrNumberOfDaysInMonth.map((day, dayNumber) => {
+						const dateToCompare = `${dayNumber + 1}.${currMonth}.${currYear}`
+						const listOfEventsInDay = events.filter(event => event.day === dateToCompare)
 
-					return (
-						<button
-							className={`
+						return (
+							<button
+								className={`
 							${
 								dayNumber + 1 === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear()
 									? 'curr-day'
@@ -82,18 +83,21 @@ const Calendar = ({
 									: ''
 							}
 							`}
-							onClick={() => {
-								handleButtonDay(day)
-								setShowPopup(false)
-							}}
-							key={dayNumber}>
-							{dayNumber + 1}
-							{listOfEventsInDay.length > 0 && (
-								<span className='event-counter'>{listOfEventsInDay.length > 9 ? '9+' : listOfEventsInDay.length}</span>
-							)}
-						</button>
-					)
-				})}
+								onClick={() => {
+									handleButtonDay(day)
+									setShowPopup(false)
+								}}
+								key={dayNumber}>
+								{dayNumber + 1}
+								{listOfEventsInDay.length > 0 && (
+									<span className='event-counter'>
+										{listOfEventsInDay.length > 9 ? '9+' : listOfEventsInDay.length}
+									</span>
+								)}
+							</button>
+						)
+					})}
+				</div>
 			</div>
 		</div>
 	)
